d6e2e4591b1d81f638a93a7d5fd3e2046c6de357
Mr. Fox <fox.contas@hotmail.com>
2022-04-04 19:10:50 -0300

    added gifs in command(not finished)

--- cida/script.js
+++ cida/script.js
@@ -53,9 +53,50 @@ creatorRef.addEventListener('click', ()=>{
 //====================================================
 //       CHANGE IMAGE IN COMMANDS SECTION
 //====================================================
+const pokedex = document.querySelector('.cpokedex h1');
+const mute = document.querySelector('.cmute h1');
+
+const muteID = document.getElementById('mutegif');
 
 
+pokedex.addEventListener('click', ()=>{
+    let img = document.getElementById('pokedexgif');
+    let imageUrl = img.src;
+    img.src = "/cida/images/gifs/pokedex.gif";
+    img.src = imageUrl;
+});
+
+mute.addEventListener('click', ()=>{
+    let img = document.getElementById('pokedexgif');
+    let imageUrl = img.src;
+    img.src = "/cida/images/gifs/mute.gif";
+    img.src = imageUrl;
+});
 
 
+/*
+pokedex.addEventListener('click', ()=>{   
+    if(!pokedexID.classList.contains('screen-active')&&(muteID.classList.contains('screen-active')) ){
+    pokedexID.classList.add('screen-active');
+    muteID.classList.remove('screen-active');
+    }else if(pokedexID.classList.contains('screen-active')){
+    //nothing;
+    }else{
+        pokedexID.classList.add('screen-active');
+    }    
+});
+
+mute.addEventListener('click', ()=>{   
+    if(!muteID.classList.contains('screen-active')&&(pokedexID.classList.contains('screen-active')) ){
+    muteID.classList.add('screen-active');
+    pokedexID.classList.remove('screen-active');
+    }else if(muteID.classList.contains('screen-active')){
+    //nothing;
+    }else{
+        muteID.classList.toggle('screen-active');
+        
+    }    
+});
+*/
 
 //====================================================
